561f52faac2275d857df30610813c08201f9e203
Iron Tom <irontom233@gmail.com>
2024-07-24 13:13:13 -0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
-// import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from '../styles';
 import { services } from '../constants';
-// import { SectionWrapper } from "../hoc";
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from '../utils/motion';
+import { Tilt } from 'react-tilt';
 
 
-const ServiceCard = ({ index, title, icon }) => {
-    return(
-    <p>{title}</p>)
-}
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
@@ -42,4 +64,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
