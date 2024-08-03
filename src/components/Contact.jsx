8553a59811c2a43521026acf81db464e07cf0cc8
Iron Tom <irontom233@gmail.com>
2024-08-02 19:09:04 -0700

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,50 +18,50 @@ const Contact = () => {
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    // const { target } = e;
-    // const { name, value } = target;
+    const { target } = e;
+    const { name, value } = target;
 
-    // setForm({
-    //   ...form,
-    //   [name]: value,
-    // });
+    setForm({
+      ...form,
+      [name]: value,
+    });
   };
 
   const handleSubmit = (e) => {
-    // e.preventDefault();
-    // setLoading(true);
+    e.preventDefault();
+    setLoading(true);
 
-    // emailjs
-    //   .send(
-    //     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-    //     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-    //     {
-    //       from_name: form.name,
-    //       to_name: "JavaScript Mastery",
-    //       from_email: form.email,
-    //       to_email: "sujata@jsmastery.pro",
-    //       message: form.message,
-    //     },
-    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    //   )
-    //   .then(
-    //     () => {
-    //       setLoading(false);
-    //       alert("Thank you. I will get back to you as soon as possible.");
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Iron Tom",
+          from_email: form.email,
+          to_email: "irontom233@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
 
-    //       setForm({
-    //         name: "",
-    //         email: "",
-    //         message: "",
-    //       });
-    //     },
-    //     (error) => {
-    //       setLoading(false);
-    //       console.error(error);
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
 
-    //       alert("Ahh, something went wrong. Please try again.");
-    //     }
-    //   );
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
   };
 
   return (
